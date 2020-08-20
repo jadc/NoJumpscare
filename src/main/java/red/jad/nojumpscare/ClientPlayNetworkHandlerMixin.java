b307f18ceef2904aae46027c4308656fab30c12c
jadc <chehimj@gmail.com>
2020-08-19 23:21:52 -0600

    removed debug msg

--- src/main/java/red/jad/nojumpscare/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/red/jad/nojumpscare/ClientPlayNetworkHandlerMixin.java
@@ -1,6 +1,5 @@
 package red.jad.nojumpscare;
 
-import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.player.PlayerEntity;
@@ -9,19 +8,12 @@ import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public class ClientPlayNetworkHandlerMixin {
 	@Redirect( method = "onGameStateChange", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;addParticle(Lnet/minecraft/particle/ParticleEffect;DDDDDD)V"))
-	private void removeJumpscare(ClientWorld clientWorld, ParticleEffect parameters, double x, double y, double z, double velocityX, double velocityY, double velocityZ){
-		System.out.println("Block particle");
-	}
-
+	private void removeJumpscare(ClientWorld clientWorld, ParticleEffect parameters, double x, double y, double z, double velocityX, double velocityY, double velocityZ){ }
 	@Redirect( method = "onGameStateChange", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;playSound(Lnet/minecraft/entity/player/PlayerEntity;DDDLnet/minecraft/sound/SoundEvent;Lnet/minecraft/sound/SoundCategory;FF)V", ordinal = 2))
-	private void removeJumpscareSound(ClientWorld clientWorld, PlayerEntity player, double x, double y, double z, SoundEvent sound, SoundCategory category, float volume, float pitch){
-		System.out.println("Block sound");
-	}
+	private void removeJumpscareSound(ClientWorld clientWorld, PlayerEntity player, double x, double y, double z, SoundEvent sound, SoundCategory category, float volume, float pitch){}
 }
